74025edbfadd4135fba0ee0a4ca9d84bc364dc18
Guilherme Athayde <guiathayde303@gmail.com>
2021-05-31 10:56:41 -0300

    Rate Limiter add

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database_ignite'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      host: 'localhost',
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentx_test_'
